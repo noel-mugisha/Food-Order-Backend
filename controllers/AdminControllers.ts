e440985cba98629ad144eaa09cef8ff698d4a223
Noel <noelmugisha332@gmail.com>
2025-01-15 07:59:45 +0200

    Implement vendor creation functionality and add Vendor model

--- controllers/AdminControllers.ts
+++ controllers/AdminControllers.ts
@@ -1,9 +1,26 @@
 import {Request, Response, NextFunction} from 'express';
 import { CreateVendorInput } from '../dto/Vendor.dto';
+import {VendorModel} from '../models/VendorModel';
 
 // controller to create a vendor
 export const CreateVendor = async (req: Request, res: Response, next: NextFunction) => {
     const {name, ownerName, foodType, pincode, address, phone, email, password} = <CreateVendorInput> req.body;
+    // saving the vendor to the database
+
+    const createVendor = await VendorModel.create({
+        name,
+        address,
+        pincode,
+        foodType,
+        email,
+        password,
+        salt: '',
+        ownerName,
+        phone,
+        rating: 0,
+        serviceAvailable: false,
+        coverImages: [],
+    });
 
 };
 
