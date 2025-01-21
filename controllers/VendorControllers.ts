46a4c6e568a030642443ff8cc6df9c941c2a1b3b
Noel <noelmugisha332@gmail.com>
2025-01-21 10:15:44 +0200

    fix: Correct type errors and improve vendor creation and login controllers

--- controllers/VendorControllers.ts
+++ controllers/VendorControllers.ts
@@ -1,5 +1,26 @@
 import { Request, Response, NextFunction } from "express";
+import { VendorLoginInputs } from "../dto/Vendor.dto";
+import { VendorModel } from "../models/VendorModel";
+import { ValidatePassword } from "../utility/PasswordHashing";
 
+// controller to login a vendor
 export const VendorLogin = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    
+    const { email, password} = <VendorLoginInputs> req.body;
+    // find the existing vendor with the email
+    const existingVendor = await VendorModel.findOne({email});
+
+    if(!existingVendor) {
+        res.status(404).json({message: "The above vendor doesn't exist"});
+        return;
+    }
+
+    // validate the password
+    const existingPassword = existingVendor.password;
+    const isPasswordValid = await ValidatePassword(password, existingPassword);
+
+    if(!isPasswordValid) {
+        res.status(400).json({message: "Invalid password"});
+        return;
+    }
+    res.status(200).json(existingVendor);
 }
--- routes/VendorRoutes.ts
+++ routes/VendorRoutes.ts
@@ -1,10 +1,15 @@
 import express from 'express';
+import { VendorLogin } from '../controllers/VendorControllers';
 
 // initiating the router
 const router = express.Router();
 
 // route to login a vendor
-router.post('/login', );
+router.post('/login', VendorLogin);
+
+router.get('/profile');
+router.patch('/profile');
+router.patch('/service');
 
 
 
